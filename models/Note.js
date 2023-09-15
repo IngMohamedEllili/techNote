6ba626fe1ec40dcb19d72fd40c403dba74aa2ef9
Mohamed  ellili <mohamed.ellili@box2home.fr>
2023-09-15 15:45:29 +0200

    fix: mongoose sequence increment

--- models/Note.js
+++ models/Note.js
@@ -1,25 +1,31 @@
 const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-
-  password: {
-    type: String,
-    required: true,
-  },
-  roles: [
-    {
-      type: String,
-      default: 'Employee',
+const AutoIncrement = require('mongoose-sequence')('mongoose');
+const noteSchema = new mongoose.Schema(
+  {
+    username: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
     },
-  ],
-  active: {
-    type: Boolean,
-    default: true,
+    title: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
   },
-});
+  { timestamps: true }
+);
 
-module.exports = mongoose.model('User', userSchema)
+// noteSchema.plugin(AutoIncrement, {
+//   inc_field: 'ticket',
+//   id: 'ticketNums',
+//   start_seq: 500
+// });
+module.exports = mongoose.model('Note', noteSchema);
--- models/User.js
+++ models/User.js
@@ -1,31 +1,25 @@
 const mongoose = require('mongoose');
-const AutoIncrement = require('mongoose-sequence')('mongoose');
-const noteSchema = new mongoose.Schema(
-  {
-    username: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'User',
-    },
-    title: {
-      type: String,
-      required: true,
-    },
-    text: {
-      type: String,
-      required: true,
-    },
-    completed: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  { timestamps: true }
-);
 
-noteSchema.plugin(AutoIncrement, {
-  inc_filed: 'ticket',
-  id: 'ticketNums',
-  start_seq: 500
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+  roles: [
+    {
+      type: String,
+      default: 'Employee',
+    },
+  ],
+  active: {
+    type: Boolean,
+    default: true,
+  },
 });
-module.exports = mongoose.model('Note', noteSchema);
+
+module.exports = mongoose.model('User', userSchema)
